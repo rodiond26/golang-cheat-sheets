23e6e415dabe28da98c2a509a6233ebed397f3b7
Rodion <rodiond26@gmail.com>
2023-02-28 00:51:58 +0500

    add cards

---
+++ cards/deck.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Create a new type of deck
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (cards deck) print() {
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
