becd53c809f92ea04a7e8e3a8da0735e8418a0a1
Rodion <rodiond26@gmail.com>
2023-03-01 23:18:02 +0500

    add cards

---
+++ simple-projects/cards/deck_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if (len(d)) != 16 {
+		t.Errorf("Expected 16, but got %v", len(d))
+	}
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades, but got %v", d[0])
+	}
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	filename := "_decktesting"
+	os.Remove(filename)
+	deck := newDeck()
+	deck.saveToFile(filename)
+	loadedDeck := newDeckFromFile(filename)
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16, but got %v", len(loadedDeck))
+	}
+	os.Remove(filename)
+}
